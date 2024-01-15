8104bd1081dd8b1abeb25e761e455ba6309fb1a1
mayanapumpy231 <mayanapumpy08@gmail.com>
2024-01-15 13:06:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './components/Home'
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Quotes from './components/Quotes';
+import BookDetails from './components/bookDetail.jsx';
+import NotFound from	 './components/notFound.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div>
+			<Router>
+				<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/quotes' element={<Quotes />}></Route>
+				<Route path='Books/:id' element={<BookDetails />}></Route>
+				<Route path='*' element={<NotFound/>} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
-export default App;
+export default App
